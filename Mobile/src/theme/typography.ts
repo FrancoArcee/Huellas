52b2387f2a54e02e8a51789115094b326b6fde2f
Ramiro Gil <gilramiro49@gmail.com>
2026-05-03 12:00:41 -0300

    feat: agrego tipografías principales

---
+++ Mobile/src/theme/typography.ts
@@ -0,0 +1,52 @@
+export const typography = {
+  //Variantes de la tipografía principal
+  fontFamily: {
+    light: "Poppins_300Light",
+    regular: "Poppins_400Regular",
+    medium: "Poppins_500Medium",
+    semiBold: "Poppins_600SemiBold",
+    bold: "Poppins_700Bold",
+  },
+
+  //Variantes de tamaños y estilos para cada tipo de texto
+  variants: {
+    h1: {
+      fontFamily: "Poppins_700Bold",
+      fontSize: 32,
+      lineHeight: 40,
+    },
+    h2: {
+      fontFamily: "Poppins_700Bold",
+      fontSize: 28,
+      lineHeight: 36,
+    },
+    h3: {
+      fontFamily: "Poppins_600SemiBold",
+      fontSize: 24,
+      lineHeight: 32,
+    },
+    h4: {
+      fontFamily: "Poppins_600SemiBold",
+      fontSize: 20,
+      lineHeight: 28,
+    },
+
+    p: {
+      fontFamily: "Poppins_400Regular",
+      fontSize: 16,
+      lineHeight: 24,
+    },
+
+    body: {
+      fontFamily: "Poppins_400Regular",
+      fontSize: 14,
+      lineHeight: 22,
+    },
+
+    caption: {
+      fontFamily: "Poppins_300Light",
+      fontSize: 12,
+      lineHeight: 16,
+    },
+  },
+};
