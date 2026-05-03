b4b20da1099604c8bc2bffeb78289a42dde63394
Ramiro Gil <gilramiro49@gmail.com>
2026-05-03 12:00:05 -0300

    feat: agrego esquema de colores

---
+++ Mobile/src/theme/colors.ts
@@ -0,0 +1,26 @@
+export const colors = {
+  //Colores principales
+  primary: "#F19C2B",
+  secondary: "#9088E4",
+
+  //Escala de grises
+  white: "#FFFFFF",
+  black: "#000000",
+  gray50: "#F9FAFB",
+  gray100: "#F3F4F6",
+  gray200: "#E5E7EB",
+  gray300: "#D1D5DB",
+  gray400: "#9CA3AF",
+  gray500: "#6B7280",
+  gray600: "#4B5563",
+  gray700: "#374151",
+  gray800: "#1F2937",
+  gray900: "#111827",
+
+  background: "#F1EEEE",
+
+  //Texto
+  textPrimary: "#111827",
+  textSecondary: "#6B7280",
+
+};
